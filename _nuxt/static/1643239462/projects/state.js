a46583b771446b2483cd4361014937a24e28b245
emretepedev <emretepedev@users.noreply.github.com>
2022-01-27 02:11:32 +0000

    deploy: 454322d19b0d34ede0425401270da26f20ea61c4

--- _nuxt/static/1643239462/projects/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1643239462",layout:"default",error:a,state:{},serverRendered:true,routePath:"\u002Fprojects",config:{spaName:"@emretepedev",spaUrl:"https:\u002F\u002Femretepedev.github.io",_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}},globalRefs:{}}}(null));
