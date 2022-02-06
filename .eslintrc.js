66a4b4dc77343cb49659084b632c42bdd3c0021e
emretepedev <emretepedev@gmail.com>
2022-02-06 00:09:32 +0000

    fix eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,20 @@
 module.exports = {
     root: true,
+
     env: {
         browser: true,
         node: true,
     },
+
     parserOptions: {
         parser: '@babel/eslint-parser',
         requireConfigFile: false,
     },
+
     extends: ['@nuxtjs', 'prettier', 'plugin:nuxt/recommended'],
+
     plugins: ['prettier'],
+
     // add your custom rules here
     rules: {
         'vue/multi-word-component-names': 'off',
