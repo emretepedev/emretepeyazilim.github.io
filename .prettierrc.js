56a205b6a53db1c548c033c8bee89df392656464
Emre Tepe <emretepedev@gmail.com>
2022-03-11 22:04:24 +0000

    fixed prettier config

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,13 +1,13 @@
 module.exports = {
   singleQuote: true,
-  'editor.formatOnSave': true,
   semi: false,
-  singleQuote: true,
   tabWidth: 2,
   trailingComma: 'es5',
   bracketSpacing: true,
   arrowParens: 'always',
   bracketSameLine: false,
+  useTabs: false,
   printWidth: 80,
   vueIndentScriptAndStyle: true,
+  quoteProps: 'as-needed',
 }
