3b32e7d62bde868e50a7d578dee982f2e4a95e79
emretepedev <emretepedev@gmail.com>
2022-04-26 11:35:10 +0000

    removed unnecessary social media

--- data/components/footer.js
+++ data/components/footer.js
@@ -1,16 +1,11 @@
 import {
   mdiDiscord,
   mdiEmail,
-  mdiFacebook,
   mdiGithub,
   mdiGitlab,
-  mdiInstagram,
   mdiLinkedin,
-  mdiPhone,
   mdiSend,
-  mdiSkype,
   mdiTwitter,
-  mdiWhatsapp,
 } from '@mdi/js'
 
 export default {
@@ -36,24 +31,9 @@ export default {
       color: '#1DA1F2',
     },
     {
-      href: 'https://instagr.am/emretepedev/',
-      image: mdiInstagram,
-      color: '#E4405F',
-    },
-    {
-      href: 'https://fb.me/emretepedev/',
-      image: mdiFacebook,
-      color: '#1877F2',
-    },
-    {
       href: 'https://t.me/emretepedev/',
       image: mdiSend,
       color: '#26A5E4',
-    },
-    {
-      href: 'https://wa.me/905522801804?text=Hi%20@emretepedev.',
-      image: mdiWhatsapp,
-      color: '#25D366',
     },
     {
       href: 'https://discordapp.com/users/365827212890341378/',
@@ -61,18 +41,8 @@ export default {
       color: '#5865F2',
     },
     {
-      href: 'skype:live:.cid.ebf04f9e644e1570',
-      image: mdiSkype,
-      color: '#00AFF0',
-    },
-    {
       href: 'mailto:emretepedev@gmail.com',
       image: mdiEmail,
-      color: '#FFFFFF',
-    },
-    {
-      href: 'tel:+905522801804',
-      image: mdiPhone,
       color: '#FFFFFF',
     },
   ],
